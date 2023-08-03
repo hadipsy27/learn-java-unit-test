324af79767b85463ebd7b8a007c47a279a33068c
hadipsy27 <hadipsy27@gmail.com>
2023-08-03 14:17:42 +0700

    Junit : Menggunakan Assertions

--- src/main/java/learn/labs/test/Calculator.java
+++ src/main/java/learn/labs/test/Calculator.java
@@ -2,6 +2,14 @@ package learn.labs.test;
 
 public class Calculator {
 
+    public Integer devide(Integer fist, Integer second){
+        if(second == 0){
+            throw new IllegalArgumentException("Cannot divide by zero");
+        } else {
+            return fist / second;
+        }
+    }
+
     public Integer add(Integer first, Integer second) {
         return first + second;
     }
--- src/test/java/learn/labs/test/CalculatorTest.java
+++ src/test/java/learn/labs/test/CalculatorTest.java
@@ -1,13 +1,36 @@
 package learn.labs.test;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    private Calculator calculator = new Calculator();
+    private final Calculator calculator = new Calculator();
 
     @Test
     public void testAddSuccess(){
         var result = calculator.add(10, 10);
+
+        // cara manual
+//        if(result != 20){
+//            throw  new RuntimeException("Test Gagal!");
+//        }
+
+        // Menggunakan Assertion
+        assertEquals(20, result);
+    }
+
+    @Test
+    public void testDevideSuccess(){
+        var result = calculator.devide(100, 10);
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void testDevideFailed(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.devide(100, 0);
+        });
+
     }
 }
